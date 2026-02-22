9f43c308ffb9e973eb4ae6df42688eef1bb8176b
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-02-22 16:07:17 -0500

    Add function for adding a new book to the empty array by using the .push() method.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+let myLibrary = [];
+
+// Constructor
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.info = function () {
-    return title + author + ", " + pages;
+    return title + author + ", " + pages + ", " + read;
   };
+}
+
+function addBookToLibrary(Title, Author, Pages, Read) {
+  let book = new Book(Title, Author, Pages, Read);
+
+  // Pushing contents of book to myLibrary array
+  myLibrary.push(book);
 }
