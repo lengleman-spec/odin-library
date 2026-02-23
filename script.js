36c9f8e490327f469ace5544196360501dfa0a96
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-02-23 11:16:41 -0500

    Add logic to see if checkbox is checked and add book to array.

--- script.js
+++ script.js
@@ -54,7 +54,9 @@ bookForm.addEventListener("submit", function (event) {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
-  const read = document.getElementById("read").value;
+  const read = document.getElementById("read").value.checked
+    ? "Read"
+    : "Not Read";
 
   addBookToLibrary(title, author, pages, read);
   displayBooksOnPage();
